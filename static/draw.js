2bc39f0df73e180a25acc358bfb1999f04420abd
Terkwood <metaterkhorn@gmail.com>
2019-01-08 10:02:06 -0500

    replace starting image with text instructions

--- static/draw.js
+++ static/draw.js
@@ -1,11 +1,31 @@
-var checkExist = setInterval(function() {
-    if (document.getElementById("canvas")) {
-       clearInterval(checkExist);
-       draw(null, null);
+const FONT = "30px Arial";
+const GREEN = "rgb(0,255,0)";
+const HUD_X = 50;
+const HUD_Y = 50;
+
+var readyCheck = setInterval(function() {
+    var canvas = document.querySelector("#canvas");
+    if (canvas) {
+        clearInterval(readyCheck);
+        
+        var myCanvas = alterHiDPICanvas(canvas, document.body.clientWidth, document.body.clientHeight);
+        var ctx = myCanvas.getContext("2d");
+        ctx.beginPath();
+        ctx.rect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "black";
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.font = "24px Arial";
+        ctx.fillStyle = GREEN;
+        ctx.fillText("TAP to start.", HUD_X / 3, HUD_Y);
+        ctx.fillText("Then TAP to take a photo.", HUD_X / 3, HUD_Y * 2);
+        ctx.fillText("Photo processing may take", HUD_X / 3, HUD_Y * 4);
+        ctx.fillText("       up to 10 seconds!", HUD_X / 3, HUD_Y * 5);
     }
  }, 50);
 
-function drawCamera(camPositionCb, cameraClickCb) {
+function _drawCamera(camPositionCb, cameraClickCb) {
     var canvas = document.querySelector("#canvas");
     var ctx = canvas.getContext("2d");
     var img = new Image();
@@ -55,11 +75,6 @@ function registerCameraEvents(canvas, img, dx, dy, cameraClickCb) {
     };
 }
 
-const FONT = "30px Arial";
-const GREEN = "rgb(0,255,0)";
-const HUD_X = 50;
-const HUD_Y = 50;
-
 function snapshotBoundingBoxes(img) {
     cocoSsd.load().then(model => {
         model.detect(img).then(predictions => {
@@ -89,8 +104,6 @@ function snapshotBoundingBoxes(img) {
                 const TEXT_OFFSET = -10;
                 ctx.fillText(p.class, p.bbox[0], p.bbox[1] + TEXT_OFFSET);
             });
-
-            //drawCamera(canvas, null, cameraClickCb);
         });
     });
 }
@@ -110,13 +123,11 @@ function drawBoundingBoxes(canvas, img, cameraClickCb) {
                 ctx.rect(p.bbox[0], p.bbox[1], p.bbox[2], p.bbox[3]);
                 ctx.stroke();
 
-                ctx.font = "30px Arial";
+                ctx.font = FONT;
                 ctx.fillStyle = color;
                 const TEXT_OFFSET = -10;
                 ctx.fillText(p.class, p.bbox[0], p.bbox[1] + TEXT_OFFSET);
             });
-
-            //drawCamera(canvas, null, cameraClickCb);
         });
     });
 }
@@ -164,4 +175,28 @@ function stopVideo(video) {
     video.srcObject.getTracks()[0].stop();
     video.removeAttribute('src');
     video.load();
+}
+
+// from https://stackoverflow.com/questions/15661339/how-do-i-fix-blurry-text-in-my-html5-canvas
+var PIXEL_RATIO = (function () {
+    var ctx = document.createElement("canvas").getContext("2d"),
+        dpr = window.devicePixelRatio || 1,
+        bsr = ctx.webkitBackingStorePixelRatio ||
+              ctx.mozBackingStorePixelRatio ||
+              ctx.msBackingStorePixelRatio ||
+              ctx.oBackingStorePixelRatio ||
+              ctx.backingStorePixelRatio || 1;
+
+    return dpr / bsr;
+})();
+
+
+alterHiDPICanvas = function(can, w, h, ratio) {
+    if (!ratio) { ratio = PIXEL_RATIO; }
+    can.width = w * ratio;
+    can.height = h * ratio;
+    can.style.width = w + "px";
+    can.style.height = h + "px";
+    can.getContext("2d").setTransform(ratio, 0, 0, ratio, 0, 0);
+    return can;
 }
