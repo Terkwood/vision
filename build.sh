faf6b3753e35a9e81958fcc65752d6d7caa11418
Terkwood <metaterkhorn@gmail.com>
2019-01-15 15:25:07 -0500

    build in release mode

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cargo web build --target=wasm32-unknown-unknown
+cargo web build --target=wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/debug/vision.wasm static/.
 cp target/wasm32-unknown-unknown/debug/vision.js static/.
